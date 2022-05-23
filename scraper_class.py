66ef9a838b0d166f114dfa9d72bfc521d3890160
Shruti Turner <shrutiturner27@gmail.com>
2022-05-23 18:37:42 +0100

    Updates scraper_class.py to accept cookies.
    Code runs, but exception handling is patchy. Needs to be more robust.

--- scraper_class.py
+++ scraper_class.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from time import sleep
@@ -9,12 +10,29 @@ class Scraper:
         self.driver = webdriver.Chrome()
 
     
-    def goto_url(self, url):
+    def load_page(self, url):
         self.driver.get(url)
+
+        # going to have to deal with popups, as justgiving has them sometimes when changing page - most close with esc
+        sleep(2) # there is a slight delay for cookie frame to appear on justgiving
+        self.accept_cookies()
+
+
+    def accept_cookies(self):
+        try:
+            accept_button = self.driver.find_element(by=By.XPATH, value='//*[@id="accept-all-Cookies"]')
+            accept_button.click()
+
+        # need proper exception handling here
+        except Exception as e:
+            print('Unable to find accept button')
+            print(e)
+            pass
 
 
     def run(self):
-        self.goto_url('https://www.justgiving.com')
+        self.load_page('https://www.justgiving.com')
+        
 
 
 scraper = Scraper()
