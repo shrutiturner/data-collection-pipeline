7e2355a0d8ae36b51c8b92740a93b65c736cd53e
Shruti Turner <shrutiturner27@gmail.com>
2022-05-25 18:33:18 +0100

    Adds find_category_urls function in Scaper class.
    Updates functions to include output type in declaration.

--- scraper_class.py
+++ scraper_class.py
@@ -1,4 +1,6 @@
 import logging
+from nis import cat
+from unicodedata import category
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -12,7 +14,7 @@ class Logger:
         pass
 
     @staticmethod
-    def log_error(exception):
+    def log_error(exception) -> None:
         with open('error_log.txt', 'a') as file:
             file.write(exception)
 
@@ -21,7 +23,7 @@ class Scraper:
     def __init__(self):
         self.driver = webdriver.Chrome()
     
-    def load_page(self, url):
+    def load_page(self, url) -> None:
         self.driver.get(url)
 
         # going to have to deal with popups, as justgiving has them sometimes when changing page - most close with esc
@@ -29,20 +31,30 @@ class Scraper:
         self.accept_cookies()
 
 
-    def accept_cookies(self):
+    def accept_cookies(self) -> None:
         try:
             accept_button = self.driver.find_element(by=By.XPATH, value='//*[@id="accept-all-Cookies"]')
             accept_button.click()
 
-        # need proper exception handling here
         except Exception as e:
             Logger.log_error(e)
             pass
 
+    
+    def find_category_urls(self) -> list:
+
+        categories_location = '//*[@id="app"]/main/div[4]/div/section/h2//following-sibling::ul//descendant::a'
+
+        categories = self.driver.find_elements(by=By.XPATH, value=categories_location)
+
+        category_urls = [category.get_attribute('href') for category in categories]
+
+        return(category_urls)
+
 
     def run(self):
         self.load_page('https://www.justgiving.com')
-        
+        self.find_category_urls()
 
 
 scraper = Scraper()
