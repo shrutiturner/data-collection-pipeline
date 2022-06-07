2b36e25b763245f5f53d94c46b24e0373f82ea0d
Shruti Turner <shrutiturner27@gmail.com>
2022-06-07 17:07:04 +0100

    Refactors for clarity and optimisation.

--- scraper_class.py
+++ scraper_class.py
@@ -1,26 +1,14 @@
 import json
-import logging
-from nis import cat
 import os
-from unicodedata import category
 
+from logger import Logger
 from math import ceil
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
+from unicodedata import category
 from urllib.request import urlretrieve
 from uuid import uuid4
-
-
-
-class Logger:
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def log_error(exception) -> None:
-        with open('error_log.txt', 'a') as file:
-            file.write(exception)
 
 
 class Scraper:
@@ -33,29 +21,45 @@ class Scraper:
 
         sleep(2) # there is a slight delay for cookie frame to appear on justgiving
         
-        try:
-            self.accept_cookies()
-        except:
-            pass
+        self.accept_cookies()
+
+        return None
+
+
+    def get_elements(self, locator) -> list:
+        
+        elements = self.driver.find_elements(by=By.XPATH, value=locator)
+
+        return elements
+
+    
+    def get_element(self, locator) -> object:
+        
+        element = self.driver.find_element(by=By.XPATH, value=locator)
+
+        return element
 
 
     def accept_cookies(self) -> None:
         try:
-            accept_button = self.driver.find_element(by=By.XPATH, value='//*[@id="accept-all-Cookies"]')
+            accept_button = self.get_element('//*[@id="accept-all-Cookies"]')
             accept_button.click()
+
+        except AttributeError:
+            pass # if accept cookies button isn't there, move on
 
         except Exception as e:
             Logger.log_error(str(e))
             pass
-
     
+
     def get_category_urls(self) -> list:
 
         categories_locator = "//*[text()='Browse by fundraising category']//following-sibling::ul//descendant::a"
-        categories = self.driver.find_elements(by=By.XPATH, value=categories_locator)
+        categories = self.get_elements(categories_locator)
         category_urls = [{'category': category.text, 'url': category.get_attribute('href')} for category in categories]
 
-        return(category_urls)
+        return category_urls
 
 
     def get_fundraiser_urls(self, n) -> list:
@@ -66,7 +70,7 @@ class Scraper:
             count = 0
 
             load_more_locator = "//*[@data-testid='jg-search-load-next-page--fundraiser']"
-            load_more_button = self.driver.find_element(by=By.XPATH, value=load_more_locator)
+            load_more_button = self.get_element(load_more_locator)
 
             while count < num_clicks:
                 sleep(2)
@@ -74,85 +78,98 @@ class Scraper:
                     load_more_button.click()
                     count += 1
                 except AttributeError:
-                    print("There are no more fundraisers!")
+                    Logger.log_error("There are no more fundraisers!")
                     break
+                except Exception as e:
+                    Logger.log_error(str(e))
+                    pass
         
         fundraisers_locator = "//*[text()='Fundraisers']/parent::div//following-sibling::div//descendant::a"
-        fundraisers = self.driver.find_elements(by=By.XPATH, value=fundraisers_locator)
+        fundraisers = self.get_elements(fundraisers_locator)
         fundraiser_urls = [fundraiser.get_attribute('href') for fundraiser in fundraisers][:n]
 
-        return(fundraiser_urls)
+        return fundraiser_urls
 
 
-    def get_identifiers(self, url_string) -> str:
+    def get_slug(self, url_string) -> str:
 
-        dict_id = url_string.rpartition('/')[-1]
+        slug = url_string.rpartition('/')[-1]
 
-        return(dict_id)
+        return slug
 
 
     def get_fundraisers(self, category, num_pages) -> list:
 
         fundraiser_urls = self.get_fundraiser_urls(num_pages)
-        fundraisers = [{'slug': self.get_identifiers(url), 'url': url, 'category': category, 'id' : str(uuid4())} for url in fundraiser_urls]
+        fundraisers = [{'slug': self.get_slug(url), 'url': url, 'category': category, 'id' : str(uuid4())} for url in fundraiser_urls]
 
-        return(fundraisers)
+        return fundraisers
 
 
     def get_fundraiser_charity(self) -> str:
         try:
             charity_locator = "(//*[@data-qa='relationship-name-link'])[1]"
-            charity = self.driver.find_element(by=By.XPATH, value=charity_locator)
+            charity = self.get_element(charity_locator)
 
-            return(charity.text)
+            return charity.text
 
-        except:
-            return(None)
+        except Exception as e:
+            Logger.log_error(str(e))
+
+            return None
 
 
     def get_fundraiser_total(self) -> str:
         try:
             total_locator = "(//*[@data-qa='totaliser-total'])"
-            total = self.driver.find_element(by=By.XPATH, value=total_locator)
+            total = self.get_element(total_locator)
 
-            return(total.text.replace('£', ''))
+            return total.text.replace('£', '')
 
-        except:
-            return(None)
+        except Exception as e:
+            Logger.log_error(str(e))
+
+            return None
 
 
     def get_fundraiser_donor_num(self) -> str:
         try:
             donor_num_locator = "(//*[@data-qa='totaliser-message']/span[contains(text(), 'supporter')])"
-            donor_num = self.driver.find_element(by=By.XPATH, value=donor_num_locator)
+            donor_num = self.get_element(donor_num_locator)
             number = donor_num.text.rpartition(' ')[0] # remove the ' supporter(s)' and get only number
 
-            return(number)
+            return number
 
-        except:
-            return(None)
+        except Exception as e:
+            Logger.log_error(str(e))
+
+            return None
 
 
     def get_fundraiser_image_url(self) -> str:
         try:
             image_locator = "(//*[@data-qa='app']//img)[1]"
-            image_url = self.driver.find_element(by=By.XPATH, value=image_locator).get_attribute('src')
+            image_url = self.get_element(image_locator).get_attribute('src')
 
-            return(image_url)
+            return image_url
 
-        except:
-            return(None)
+        except Exception as e:
+            Logger.log_error(str(e))
+
+            return None
 
     
     def get_charity_image_url(self) -> str:
         try:
             charity_image_locator = "(//*[@data-qa='owner-logo']//img)[1]"            
-            charity_image_url = self.driver.find_element(by=By.XPATH, value=charity_image_locator).get_attribute('src')
+            charity_image_url = self.get_element(charity_image_locator).get_attribute('src')
 
-            return(charity_image_url)
+            return charity_image_url
 
-        except:
-            return(None)
+        except Exception as e:
+            Logger.log_error(str(e))
+
+            return None
 
     
     def get_fundraiser_info(self) -> dict:
@@ -161,7 +178,7 @@ class Scraper:
         'charity_image': self.get_charity_image_url(), 'total': self.get_fundraiser_total(), 
         'donor_num': self.get_fundraiser_donor_num(), 'fundraiser_image': self.get_fundraiser_image_url()}
 
-        return(data)
+        return data
 
     
     def write_json(self, data, path) -> None:
@@ -174,9 +191,14 @@ class Scraper:
     
     def download_image(self, path, image_url, image_name) -> None:
 
-        urlretrieve(image_url, path + '/' + image_name)
+        try:
+            urlretrieve(image_url, path + '/' + image_name)
+        
+        except Exception as e:
+            Logger.log_error(str(e))
 
-        return(None)
+        finally:
+            return None
 
 
 
@@ -207,6 +229,7 @@ if __name__ == "__main__":
             scraper.download_image(path, fundraiser['charity_image'], 'charity_image.jpg')
             scraper.download_image(path, fundraiser['fundraiser_image'], 'fundraiser_image.jpg')
         
-    
+    except:
+
     finally:
         scraper.driver.close()
