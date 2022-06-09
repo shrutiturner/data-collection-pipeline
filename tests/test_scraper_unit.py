3bb77f8242de7be7f21715e585e9a44f71ed03ba
Shruti Turner <shrutiturner27@gmail.com>
2022-06-09 17:49:43 +0100

    Writes unit and e2e tests for scraper class.
    Renamed project to src.
    
    Minor refactors to make things work.

---
+++ tests/test_scraper_unit.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append("..")
+
+import unittest
+
+from src.scraper import Scraper
+from unittest.mock import patch, MagicMock
+
+class ScraperTestCase(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.scraper = Scraper()
+
+
+    @patch('urllib.request.urlretrieve')
+    def test_download_image(self, mock_urlretrieve):
+        mock_urlretrieve.return_value = MagicMock()
+
+        self.scraper.download_image('test-image-path', 'test-image-url', 'test-image-name')
+
+        mock_urlretrieve.assert_called_once_with('test-image-url', 'test-image-path/test-image-name')
+
+
+    def tearDown(self) -> None:
+        self.scraper.driver.close()
+
+unittest.main(argv=[''], verbosity=0, exit=False)
